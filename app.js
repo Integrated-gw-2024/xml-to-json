0d8b7ccc102180df1dba3706bade07ce96ef0fce
Eito Kochi <kochi.8@outlook.jp>
2023-11-12 17:35:47 +0900

    [Change] 連番のsvgを読み込み/書き出しするようにした

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const fs = require("fs");
 const xml2js = require("xml2js");
 
-
 var parseString = require("xml2js").parseString;
-const xml = "./xml/D01.xml";
 
-let xmlData = fs.readFileSync(xml,"utf-8");
-console.log(xmlData);
-let jsonData;
+for (let i = 1; i <= 8; i++) {
+    let fileName = `アセット ${i}`;
+    const xml = fileName + ".svg";
+    let xmlData = fs.readFileSync(xml, "utf-8");
 
-parseString(xmlData, function(err,result){
-    jsonData = JSON.stringify(result,null,2);
-});
+    let jsonData;
+    parseString(xmlData, function (err, result) {
+        jsonData = JSON.stringify(result, null, 2);
+    });
 
-
-fs.writeFile("output.json",jsonData,(err) => {
-    if(err){
-        console.error(err);
-    }else{
-        console.log("saved");
-    }
-});
+    fs.writeFile("output/" + fileName + ".json", jsonData, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("saved");
+        }
+    });
+}
